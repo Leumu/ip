90bea26fe6c145b106e6e495d694c05decd74a95
Samuel <User@MacBook-Air-42.local>
2025-08-22 04:54:42 +0800

    Changed name and added some text

---
+++ src/main/java/Airy.java
@@ -0,0 +1,8 @@
+public class Airy {
+    public static void main(String[] args) {
+        String name = "Airy";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
