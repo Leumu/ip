6d7ebf2ffb067004ea35aade99b866004b020166
Samuel <User@MacBook-Air-42.local>
2025-08-22 14:50:18 +0800

    Added mark as Done logic and Task class to handle task logic

--- src/main/java/Airy.java
+++ src/main/java/Airy.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Airy {
     public static void main(String[] args) {
         String name = "Airy";
-        Scanner sc = new Scanner(System.in);
         String repeat;
         String lower;
-        // To store tasks
-        String[] tasks = new String[100];
+
+        // Detect input
+        Scanner sc = new Scanner(System.in);
+        // To store Task objects
+        Task[] tasks = new Task[100];
         // To take note of increment for the tasks array
         int increment = 0;
 
@@ -22,14 +24,30 @@ public class Airy {
             if (lower.equals("bye")) {
                 break;
             } else if (lower.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 // Print out saved tasks
                 for (int i = 0; i < increment; i++) {
-                    System.out.printf("%d. %s\n", i + 1, tasks[i]);
+                    Task taskObj = tasks[i];
+                    System.out.printf("%d.[%s] %s\n", i + 1, taskObj.getStatus(), taskObj.getTaskName());
                 }
-                System.out.println("\n");
+                System.out.print("\n");
+            } else if (lower.startsWith("mark ")) {
+                // Gets the number after mark, -1 cause array is 0 indexed
+                int taskNum = Integer.parseInt(lower.substring(5)) - 1;
+                Task taskObj = tasks[taskNum];
+                taskObj.markCompleted();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf("[%s] %s\n\n", taskObj.getStatus(), taskObj.getTaskName());
+            } else if (lower.startsWith("unmark ")) {
+                // Gets the number after unmark, -1 cause array is 0 indexed
+                int taskNum = Integer.parseInt(lower.substring(7)) - 1;
+                Task taskObj = tasks[taskNum];
+                taskObj.markUncompleted();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.printf("[%s] %s\n\n", taskObj.getStatus(), taskObj.getTaskName());
             } else {
-                // Save tasks into array
-                tasks[increment] = repeat;
+                // Save Task object into array
+                tasks[increment] = new Task(repeat);
                 increment++;
                 // Show user task has been added
                 System.out.printf("added: %s\n\n", repeat);
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+/**
+ * Task object to keep track of task name and status
+ */
+public class Task {
+    private String taskName;
+    private Boolean isCompleted;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isCompleted = false;
+    }
+
+    public String getStatus() {
+        return (isCompleted ? "X" : " ");
+    }
+
+    public void markCompleted() {
+        isCompleted = true;
+    }
+
+    public void markUncompleted() {
+        isCompleted = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+}
