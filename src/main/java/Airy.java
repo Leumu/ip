fb390ca7e8cf4635972708a11b44cb1360999134
Samuel <User@MacBook-Air-42.local>
2025-08-22 13:46:40 +0800

    Added repeat logic to repeat what they say till user says by

--- src/main/java/Airy.java
+++ src/main/java/Airy.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Airy {
     public static void main(String[] args) {
         String name = "Airy";
+        Scanner sc = new Scanner(System.in);
+        String repeat;
+
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?\n");
+
+        do {
+            repeat = sc.nextLine();
+
+            // Only repeat if it's not bye
+            if (!repeat.toLowerCase().equals("bye")) {
+                System.out.printf("%s\n\n", repeat);
+            }
+        } while(!repeat.toLowerCase().equals("bye"));
+
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
